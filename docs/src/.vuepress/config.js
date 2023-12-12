5bae1dc08c31d2b5989fecb28f695b476c05dc8b
Maxime Simoncelli <36240090+maximesimoncelli@users.noreply.github.com>
2023-12-12 09:57:06 +0100

    feat: add prettierc

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,9 +1,9 @@
-const { description } = require("../../package");
+const { description } = require('../../package');
 
 module.exports = {
-  title: "Prestashop Integration Framework",
-  description: "",
-  theme: "",
+  title: 'Prestashop Integration Framework',
+  description: '',
+  theme: '',
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
@@ -12,56 +12,50 @@ module.exports = {
    */
   head: [
     [
-      "link",
+      'link',
       {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/assets/images/favicons/apple-touch-icon.png",
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/assets/images/favicons/apple-touch-icon.png',
       },
     ],
     [
-      "link",
+      'link',
       {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/assets/images/favicons/favicon-32x32.png",
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/assets/images/favicons/favicon-32x32.png',
       },
     ],
     [
-      "link",
+      'link',
       {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/assets/images/favicons/favicon-16x16.png",
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/assets/images/favicons/favicon-16x16.png',
       },
     ],
+    ['link', { rel: 'manifest', href: '/assets/images/favicons/site.webmanifest' }],
     [
-      "link",
-      { rel: "manifest", href: "/assets/images/favicons/site.webmanifest" },
-    ],
-    [
-      "link",
+      'link',
       {
-        rel: "mask-icon",
-        href: "/assets/images/favicons/safari-pinned-tab.svg",
-        color: "#3a0839",
+        rel: 'mask-icon',
+        href: '/assets/images/favicons/safari-pinned-tab.svg',
+        color: '#3a0839',
       },
     ],
+    ['link', { rel: 'shortcut icon', href: '/assets/images/favicons/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#011638' }],
     [
-      "link",
-      { rel: "shortcut icon", href: "/assets/images/favicons/favicon.ico" },
-    ],
-    ["meta", { name: "msapplication-TileColor", content: "#011638" }],
-    [
-      "meta",
+      'meta',
       {
-        name: "msapplication-config",
-        content: "/assets/images/favicons/browserconfig.xml",
+        name: 'msapplication-config',
+        content: '/assets/images/favicons/browserconfig.xml',
       },
     ],
-    ["meta", { name: "theme-color", content: "#011638" }],
+    ['meta', { name: 'theme-color', content: '#011638' }],
   ],
 
   /**
@@ -70,139 +64,139 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: "",
+    repo: '',
     editLinks: false,
-    docsDir: "",
-    editLinkText: "",
+    docsDir: '',
+    editLinkText: '',
     lastUpdated: false,
     displayAllHeaders: true,
     sidebarDepth: 1,
     nav: [
-      { text: "Home", link: "/" },
+      { text: 'Home', link: '/' },
       {
-        text: "Standard Module Development References",
-        ariaLabel: "Standard Module Development References",
+        text: 'Standard Module Development References',
+        ariaLabel: 'Standard Module Development References',
         items: [
           {
-            text: "Module Developer Guide",
-            link: "https://devdocs.prestashop-project.org/",
+            text: 'Module Developer Guide',
+            link: 'https://devdocs.prestashop-project.org/',
           },
           {
-            text: "Module Generator",
-            link: "https://validator.prestashop.com/generator",
+            text: 'Module Generator',
+            link: 'https://validator.prestashop.com/generator',
           },
           {
-            text: "Built For module example",
-            link: "https://github.com/PrestaShopCorp/builtforjsexample",
+            text: 'Built For module example',
+            link: 'https://github.com/PrestaShopCorp/builtforjsexample',
           },
         ],
       },
-      { text: "PrestaShop Documentation", link: "https://doc.prestashop.com/" },
+      { text: 'PrestaShop Documentation', link: 'https://doc.prestashop.com/' },
     ],
     sidebar: [
-      { title: "Introduction", path: "0-introduction/" },
-      { title: "How It Works", path: "/1-how-it-works/" },
+      { title: 'Introduction', path: '0-introduction/' },
+      { title: 'How It Works', path: '/1-how-it-works/' },
       {
-        title: "Before Starting",
+        title: 'Before Starting',
         //path: '/2-before-starting/',
         sidebarDepth: 0,
         collapsable: false,
         initialOpenGroupIndex: 0,
         children: [
           {
-            path: "/2-before-starting/#available-tools",
-            title: "Available Tools",
+            path: '/2-before-starting/#available-tools',
+            title: 'Available Tools',
           },
           {
-            path: "/2-before-starting/#preparing-for-technical-validation",
-            title: "Preparing for Technical Validation",
+            path: '/2-before-starting/#preparing-for-technical-validation',
+            title: 'Preparing for Technical Validation',
           },
           {
-            path: "https://meetings.hubspot.com/esteban-martin3/prestashop-new-framework-integration-meeting?utm_campaign=documentation&utm_source=sidebar&utm_medium=link",
-            title: "Plan my Built For PrestaShop development",
+            path: 'https://meetings.hubspot.com/esteban-martin3/prestashop-new-framework-integration-meeting?utm_campaign=documentation&utm_source=sidebar&utm_medium=link',
+            title: 'Plan my Built For PrestaShop development',
           },
         ],
       },
       {
-        title: "Preparing Your Environment",
-        path: "/3-preparing-your-environment/",
+        title: 'Preparing Your Environment',
+        path: '/3-preparing-your-environment/',
       },
       {
-        title: "Integrating PrestaShop Account",
-        path: "/4-prestashop-account/",
+        title: 'Integrating PrestaShop Account',
+        path: '/4-prestashop-account/',
       },
       {
-        title: "PrestaShop Billing",
+        title: 'PrestaShop Billing',
         sidebarDepth: 0,
         collapsable: false,
         initialOpenGroupIndex: 0,
         children: [
           {
-            path: "/5-prestashop-billing/1-overview/",
-            title: "Overview",
+            path: '/5-prestashop-billing/1-overview/',
+            title: 'Overview',
           },
-          "/5-prestashop-billing/2-concepts/",
-          "/5-prestashop-billing/3-tutorial/",
+          '/5-prestashop-billing/2-concepts/',
+          '/5-prestashop-billing/3-tutorial/',
           {
-            title: "How-Tos",
+            title: 'How-Tos',
             sidebarDepth: 0,
             collapsable: false,
             children: [
-              "/5-prestashop-billing/4-how-tos/1-stairstep/",
-              "/5-prestashop-billing/4-how-tos/2-display-invoice-pane/",
-              "/5-prestashop-billing/4-how-tos/3-customize-cancellation/",
-              "/5-prestashop-billing/4-how-tos/4-handle-plan-selection/",
-              "/5-prestashop-billing/4-how-tos/5-retrieve-subscription/",
-              "/5-prestashop-billing/4-how-tos/6-multiple-billing-period/",
+              '/5-prestashop-billing/4-how-tos/1-stairstep/',
+              '/5-prestashop-billing/4-how-tos/2-display-invoice-pane/',
+              '/5-prestashop-billing/4-how-tos/3-customize-cancellation/',
+              '/5-prestashop-billing/4-how-tos/4-handle-plan-selection/',
+              '/5-prestashop-billing/4-how-tos/5-retrieve-subscription/',
+              '/5-prestashop-billing/4-how-tos/6-multiple-billing-period/',
             ],
           },
           {
-            title: "Use Cases",
+            title: 'Use Cases',
             sidebarDepth: 0,
             collapsable: false,
             children: [
-              "/5-prestashop-billing/5-use-cases/1-flat-fee/",
-              "/5-prestashop-billing/5-use-cases/2-stair-step/",
-              "/5-prestashop-billing/5-use-cases/3-usage-based/",
-              "/5-prestashop-billing/5-use-cases/4-charge-at-term/",
+              '/5-prestashop-billing/5-use-cases/1-flat-fee/',
+              '/5-prestashop-billing/5-use-cases/2-stair-step/',
+              '/5-prestashop-billing/5-use-cases/3-usage-based/',
+              '/5-prestashop-billing/5-use-cases/4-charge-at-term/',
             ],
           },
           {
-            title: "References",
+            title: 'References',
             sidebarDepth: 0,
             collapsable: false,
             children: [
-              "/5-prestashop-billing/6-references/1-webhook/",
-              "/5-prestashop-billing/6-references/2-billing-api/",
-              "/5-prestashop-billing/6-references/3-billing-cdc/",
+              '/5-prestashop-billing/6-references/1-webhook/',
+              '/5-prestashop-billing/6-references/2-billing-api/',
+              '/5-prestashop-billing/6-references/3-billing-cdc/',
             ],
           },
-          "/5-prestashop-billing/7-faq/",
+          '/5-prestashop-billing/7-faq/',
         ],
       },
       {
-        title: "Integrating PrestaShop CloudSync",
-        path: "/7-prestashop-cloudsync/",
+        title: 'Integrating PrestaShop CloudSync',
+        path: '/7-prestashop-cloudsync/',
       },
-      { title: "APIs", path: "/8-apis/" },
-      { title: "Submitting Your Module", path: "/9-submitting-your-module/" },
-      { title: "Validation Checklist", path: "/10-validation-checklist/" },
-      { title: "FAQ", path: "/11-faq/" },
+      { title: 'APIs', path: '/8-apis/' },
+      { title: 'Submitting Your Module', path: '/9-submitting-your-module/' },
+      { title: 'Validation Checklist', path: '/10-validation-checklist/' },
+      { title: 'FAQ', path: '/11-faq/' },
       {
-        title: "Legacy Procedure for Account and Billing",
-        path: "/12-legacy-account-and-billing/",
+        title: 'Legacy Procedure for Account and Billing',
+        path: '/12-legacy-account-and-billing/',
       },
     ],
   },
 
   plugins: [
-    "code-switcher",
+    'code-switcher',
     [
-      "vuepress-plugin-medium-zoom",
+      'vuepress-plugin-medium-zoom',
       {
         delay: 200,
         options: {
-          background: "#888888",
+          background: '#888888',
           margin: 24,
           scrollOffset: 0,
         },
@@ -215,20 +209,20 @@ module.exports = {
     extendMarkdown: (md) => {
       (md.renderer.rules.image = (tokens, idx, options, env, self) => {
         const token = tokens[idx];
-        const hrefIndex = token.attrIndex("src");
+        const hrefIndex = token.attrIndex('src');
         const src = token.attrs[hrefIndex][1];
-        const classesIndex = token.attrIndex("class");
-        const classes = classesIndex >= 0 ? token.attrs[classesIndex][1] : "";
-        if (classes.indexOf("book-meeting") >= 0) {
+        const classesIndex = token.attrIndex('class');
+        const classes = classesIndex >= 0 ? token.attrs[classesIndex][1] : '';
+        if (classes.indexOf('book-meeting') >= 0) {
           return `<a href="https://meetings.hubspot.com/esteban-martin3/prestashop-new-framework-integration-meeting" target="_blank"><img src="${src}" alt="Book an appointment"></a>`;
         } else {
           return self.renderToken(tokens, idx, options);
         }
       }),
-        md.use(require("markdown-it-attrs"), {
+        md.use(require('markdown-it-attrs'), {
           // optional, these are default options
-          leftDelimiter: "{#",
-          rightDelimiter: "#}",
+          leftDelimiter: '{#',
+          rightDelimiter: '#}',
           allowedAttributes: [], // empty array = all attributes are allowed
         });
     },
