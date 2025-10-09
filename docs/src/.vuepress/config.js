46c13301b5870019e231d3369dc43f87a028f6f8
Matéo Castillo <mCastillo@Mateo-Presta.local>
2025-10-09 11:45:01 +0200

    Added google tag manager, googla analytics and axeptio script to head

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -43,6 +43,44 @@ module.exports = {
       { name: 'msapplication-config', content: '/assets/images/favicons/browserconfig.xml' },
     ],
     ['meta', { name: 'theme-color', content: '#011638' }],
+
+    // Google Tag Manager (GTM) Head Snippet
+    [
+      'script',
+      {},
+      `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PRPNJXS4');
+    `,
+    ],
+
+    // Axeptio Cookie Consent Snippet
+    [
+      'script',
+      {},
+      `
+      window.axeptioSettings = {
+        clientId: "68e52bec83b7c8abf20cbf50",
+        cookiesVersion: "docs-fr-EU",
+      };
+
+      (function(d, s) {
+        var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+        e.async = true; e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+      })(document, "script");
+    `,
+    ],
+
+    // GTM (noscript) - Placed in the head
+    [
+      'noscript',
+      {},
+      '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRPNJXS4" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+    ],
   ],
 
   themeConfig: {
